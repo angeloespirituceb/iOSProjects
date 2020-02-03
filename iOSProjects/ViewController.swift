5f33209205b9cc12be63f2ee1cd755147e0e02c4
Angelo Espiritu <angelo.espiritu@cebupacificair.com>
2020-02-03 14:53:35 +0800

    Initial Project Specification

---
+++ iOSProjects/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSProjects
+//
+//  Created by Rowan de Chavez on 2/3/20.
+//  Copyright © 2020 Cebu Pacific Air. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
